38102ce1c321d932a3b40b3a357a57584a920840
Alex Lee <hyunho98@gmail.com>
2023-01-26 17:55:30 -0500

    commit tree f2e31fd336eee412fb6606c58040dc27a0a0dc26

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  const renderP = (props.bio && props.bio.length > 0) ? <p>{props.bio}</p> : null;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {renderP}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
